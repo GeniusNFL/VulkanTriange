6a8c49919bb69559aed5613ae5a85b87655da9f9
GeniusNFL <GeniusNFL@users.noreply.github.com>
2018-06-19 23:54:06 +0800

    add shader files

---
+++ VulkanTriange/shaders/compile.bat
@@ -0,0 +1,4 @@
+//D:\Vulkan\VulkanSDK\1.1.73.0\Bin32\glslangValidator.exe -V shader.vert
+D:\Vulkan\VulkanSDK\1.1.73.0\Bin32\glslangValidator.exe -V shader.frag
+D:\Vulkan\VulkanSDK\1.1.73.0\Bin32\glslangValidator.exe -V rectangle.vert
+pause
